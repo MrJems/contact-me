383e79656a4696ffa0bfe6ab7c23920a2d5301b7
TelarKaran <telarkaran@outlook.com>
2025-01-21 23:31:07 +0530

    deleted coment

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 const config = require("./config/constants");
-const connectDB = require("./u")
+const connectDB = require("./u");
 
 const app = express();
-
-// const port = process.env.PORT || 3000;
 
 app.use(express.json());
 connectDB();
