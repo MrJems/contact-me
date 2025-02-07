6ea78fcde1fbcaab8b763c034c6a5b54190e2edb
MrJems <116582213+MrJems@users.noreply.github.com>
2025-02-07 18:03:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://contact-me-liart.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
