61b1317dd246b597513e8e9688af4aa2e7e8f515
MrJems <116582213+MrJems@users.noreply.github.com>
2025-02-28 23:22:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,15 +18,17 @@ const authorizeRoles = require("./middleware/authorizeRoles");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://contact-karan.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "https://contact-karan.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
